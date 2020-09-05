0c3199077bbdf5dc57c48594d7566eb590270a84
Ahmet Furkan Duzgider <62030299+bostonlu07@users.noreply.github.com>
2020-09-06 00:03:16 +0300

    first

---
+++ UI/Interfaces/IAirplaneUI.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MyGame
+{
+    public interface IAirplaneUI 
+    {
+        void HandleAirplaneUI();
+    }
+}
